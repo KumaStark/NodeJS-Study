dd523599073cbd8da010d1be21777aea2014d09c
KumaStark <elastic48@msn.com>
2020-08-19 18:33:48 +0800

    Chat Time Getter Modified

--- Homework/controllers/chat.js
+++ Homework/controllers/chat.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const tpl = require('../libs/tpl');
 const users = [];
-let nowTime;
+const timeFormat = "hh:mm:ss A";
 module.exports = {
     index: (ctx) => {
         ctx.body = tpl.render('chat.html');
@@ -10,14 +10,13 @@ module.exports = {
         // users.push({
         //     id: socket.id
         // });
-        
-        console.log(`用户${socket.id}进入了简易聊天室`,moment().format('hh:mm:ss A'));
+        console.log(`用户${socket.id}进入了简易聊天室`,moment().format(timeFormat));
         socket.emit('Greatings', `欢迎${socket.id}进入聊天室`);
         //通过socket通知给其它socket
         socket.broadcast.emit('Greatings', `新朋友${socket.id}加入了，我们欢迎👏他！`);
         // socket.broadcast.emit('userUpdate', users);
         socket.on('Message', data => {
-            nowTime = moment().format('hh:mm:ss A');
+            let nowTime = moment().format(timeFormat);
             socket.emit('Message', `[${nowTime}]我说：${data}`);
             socket.broadcast.emit('Message', `[${nowTime}]${socket.id} 说：${data}`);
         })
